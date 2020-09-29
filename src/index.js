dcde676ce519567db8b5be29aee61178b0d6c182
Lauri K <laurikamm@gmail.com>
2020-09-29 14:40:46 +0300

    feat: make basic mathematical functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+  let temp = array[0];
+  if (array.length) {
+    for (let item of array) {
+      if (item < temp) {
+        temp = item;
+      }
+    }
+    return temp;
+  } else if (array.length == 0) {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+  let temp = array[0];
+  if (array.length) {
+    for (let item of array) {
+      if (item > temp) {
+        temp = item;
+      }
+    }
+    return temp;
+  } else if (array.length == 0) {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {
+  let summ = 0;
+  if (array.length) {
+    for (let item of array) {
+      summ += item;
+    }
+    summ /= array.length;
+    return summ;
+  } else return 0;
 }
